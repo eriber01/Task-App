070d46d592fceff4f3e0d0cce65b422621f5c950
Eriber Tejeda <59887012+eriber01@users.noreply.github.com>
2021-09-19 19:23:24 -0400

    La app ya esta lista

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
+import './Home.css'
+
 export default function Home() {
     return (
-        <div style={{textAlign: 'center'}}>
-            Home
-            "Diseño es proceso"
+        <div className='home'>
+            <h1>Hola</h1>
+            <p>Bienvenido Su Aplicacion de Tareas</p>
+            <p>Aqui te explicare las Reglas y lo que puedes hacer:</p>
+
+            <ul>
+                <li>Puedes agregar tu Tareas pendientes con su respectivo Nombre y Descripcion</li>
+                <li>Para agregar Tareas debes estas logueado o resgistrado.</li>
+                <li>Las Tareas que Guardes no se Borraran Amenos que TU lo hagas</li>
+                <li>Solo puedes ver las tareas guardadas con tu TU Correo</li>
+                <li>Si olvidas el Correo o la Contraseña no la puedes recuperar</li>
+            </ul>
         </div>
     )
 }
